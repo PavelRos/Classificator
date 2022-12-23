da4bc31133d3261f295d7fda2453238dfa4622ad
Pavel Roslavlev <pavelros288@gmail.com>
2022-12-23 20:06:29 +0300

    Аdd missing urls

--- classificator_app/main_app/urls.py
+++ classificator_app/main_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from classificator import views as classificator_views
@@ -10,4 +10,6 @@ urlpatterns = [
     path("add_file", views.saveFileInDB),
     path("train", classificator_views.train),
     path("classificate", classificator_views.classificate),
+    re_path(r"^text/(?P<id>.+)", views.getTextFromArticle),
+    re_path(r"^remove/(?P<id>.+)", views.removeArticle),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
