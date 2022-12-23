0a56df244f5e5db52961ad4ac81038359049577c
Pavel Roslavlev <pavelros288@gmail.com>
2022-12-23 14:00:11 +0300

    Add class for classification model

--- classificator_app/classificator/models.py
+++ classificator_app/classificator/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+import pandas as pd
+import nltk
+# nltk.download()
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from pickle import dump, load
+from classificator_app.settings import MODEL_PATH
 
 # Create your models here.
+
+
+class Svc_model:
+
+    def __init__(self, model, vect, transformer):
+        self.svclassifier = model
+        self.transformer = transformer
+        self.vectorizer = vect
